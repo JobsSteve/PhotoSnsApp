1b96f030fc3f3eaeacb6f6481e3ab29db4cd8795
Yuumi Yoshida <yy@ey-office.com>
2014-06-27 09:49:16 +0900

    Initial Commit

---
+++ PhotoSnsApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PhotoSnsApp
+//
+//  Created by Yuumi Yoshida on 2014/06/27.
+//  Copyright (c) 2014年 Yuumi Yoshida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PhotoSnsApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PhotoSnsApp
+//
+//  Created by Yuumi Yoshida on 2014/06/27.
+//  Copyright (c) 2014年 Yuumi Yoshida. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
